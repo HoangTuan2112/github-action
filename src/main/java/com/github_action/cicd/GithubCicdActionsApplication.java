445968ffbba403137b72d3ce276c141efb3b08fd
HoangTuan2112 <tuanmaiyeunguoido@gmail.com>
2024-05-19 11:22:25 +0700

    update

--- src/main/java/com/github_action/cicd/GithubCicdActionsApplication.java
+++ src/main/java/com/github_action/cicd/GithubCicdActionsApplication.java
@@ -1,4 +1,4 @@
-package com.javatechie.cicd;
+package com.github_action.cicd;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
